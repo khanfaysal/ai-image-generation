b4f52a981cf79f0b1a6fbcd864fb04a4f4b32a4b
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-20 18:17:13 +0600

    api update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 		setLoading(true);
 
 		try {
-			const response = await fetch('https://ai-image-generation-3zrf.onrender.com/api/v1/post', {
+			const response = await fetch("https://ai-image-generation-3zrf.onrender.com/api/v1/post", {
 				method: 'GET',
 				headers: {
 					'Content-Type': 'application/json',
